4260ff8dbd8510816c3f1604344d35b1f89ad447
Jackson Ming Hu <huming2207@gmail.com>
2017-05-17 17:00:41 +1000

    Add a demo for tomorrow tute

---
+++ src/user_cli.c
@@ -0,0 +1,5 @@
+//
+// Created by Ming Hu on 17/5/17.
+//
+
+#include "user_cli.h"
---
+++ src/user_cli.h
@@ -0,0 +1,8 @@
+//
+// Created by Ming Hu on 17/5/17.
+//
+
+#ifndef RMITER_C_USER_CLI_H
+#define RMITER_C_USER_CLI_H
+
+#endif //RMITER_C_USER_CLI_H
